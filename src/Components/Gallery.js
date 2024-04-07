997ae5346c8c696a93a0ccb7f2fa688c3e190e17
Aaren Vo <aarenjvo@gmail.com>
2024-04-07 09:53:15 -0700

    first commit

---
+++ src/Components/Gallery.js
@@ -0,0 +1,14 @@
+import GalleryItem from './GalleryItem'
+
+function Gallery(props) {
+    const display = props.data.map((item, i) => {
+        return <GalleryItem key={i} item={item} />
+    })
+    return (
+        <div>
+            {display}
+        </div>
+    )
+}
+
+export default Gallery
